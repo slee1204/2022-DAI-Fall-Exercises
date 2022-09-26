cbd8c7a0567d7cb62a52bf07e889445b43eba971
SueLee <slee1008@my.bcit.ca>
2022-09-25 17:52:15 -0700

    create vechai ui page

--- dai_web_ex/pages/_app.js
+++ dai_web_ex/pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+import Head from 'next/head'
+import * as React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return <>
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <link
+      rel="stylesheet"
+      href="path/to/node_modules/normalize.css/normalize.css"
+      />
+
+
+    </Head>
+    
+
+    <Component {...pageProps} />
+ </>
+ }
 
 export default MyApp
---
+++ dai_web_ex/pages/sandbox/vc_ui.js
@@ -0,0 +1,10 @@
+import { VechaiProvider, Button } from "@vechaiui/react";
+
+
+export default function App() {
+  return (
+    <VechaiProvider>
+      <Button>Hello Vechai</Button>
+    </VechaiProvider>
+  );
+}
