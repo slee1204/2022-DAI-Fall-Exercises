7b8e1d65432f88c20196048704b556d5bafa872d
SueLee <slee1008@my.bcit.ca>
2022-09-27 11:39:50 -0700

    redo ex3

--- dai_web_ex/pages/_app.js
+++ dai_web_ex/pages/_app.js
@@ -1,46 +1,28 @@
-import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+import Head from 'next/head'
+import * as React from "react";
 
 function MyApp({ Component, pageProps }) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  return  <Component {...pageProps} />
+return  <Component {...pageProps} />
 
-    /*{ <Head>
-=======
-  return <>
-    <Head>
->>>>>>> ca89010a9398cc137b8c0043783297224760477f
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <link
-      rel="stylesheet"
-      href="path/to/node_modules/normalize.css/normalize.css"
-      />
+/*{ <Head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <link
+  rel="stylesheet"
+  href="path/to/node_modules/normalize.css/normalize.css"
+  />
+</Head> }*/
 
 
-<<<<<<< HEAD
-    </Head> }*/
-    
 
-    
- 
-=======
-    </Head>
-    
 
-    <Component {...pageProps} />
- </>
->>>>>>> ca89010a9398cc137b8c0043783297224760477f
- }
-=======
-  return <Component {...pageProps} />
 }
->>>>>>> parent of e62d40d (week 3 inclass)
 
 export default MyApp
