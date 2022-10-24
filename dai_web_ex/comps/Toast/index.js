3ef5c8b72f1f483d61be03e1a7dd423a2539b31a
SueLee <slee1008@my.bcit.ca>
2022-10-24 12:45:40 -0700

    oct 24-preclass

--- dai_web_ex/comps/Toast/index.js
+++ dai_web_ex/comps/Toast/index.js
@@ -1,7 +1,22 @@
-//use framer motion and timerout
+//use framer motion and timeout
+import { Message } from 'semantic-ui-react'
+import { motion } from 'framer-motion';
 
-export default function Toast(){
-  return <div>
-    This is a toast alert!
-  </div>
+
+export default function Toast({
+header="This is a header",
+content="This is a content"
+
+}){
+  return <motion.div
+              style={{textAlign: "center", marginTop: 100}}
+              initial={{ opacity: 1, scale: 0, x: -1000 }}
+              animate={{ opacity: 1, scale: 1, x:-450 }}
+              transition={{ duration: 0.5 }}>
+    <Message
+    compact
+    header={header}
+    content={content}
+  />
+  </motion.div>
 }
